283a28932ec79ddae0a8941a3c03200bacd24483
gingercook <vahitaras@gmail.com>
2021-09-23 23:49:28 -0400

    alternative props method integrated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Card from './Card';
 import contacts from '../contacts';
 
+// You can see alternative methods to pass props arguments
 function App() {
   return (
     <div>
@@ -9,6 +10,12 @@ function App() {
       <Card {...contacts[0]} />
       <Card {...contacts[1]} />
       <Card {...contacts[2]} />
+      <Card 
+        name='Michael Jackson' 
+        imgURL='https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/03/30/12/shutterstock-editorial-138187a.jpg?width=990&auto=webp&quality=75' 
+        phone='+923 584 725' 
+        email='mj@mj.com' 
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//1. Apply CSS styles to App.jsx component
-//to match the appearance on the completed app:
-//https://c6fkx.csb.app/
-//2. Extract the contact card as a reusable Card component.
-//3. Use props to render the default Beyonce contact card
-//so the Card component can be reused for other contacts.
+// Check out Card.jsx for more details
+// import contacts from '../contacts';
+// <Card {...contacts[0]} />
+// <Card {...contacts[1]} />
+// <Card {...contacts[2]} />
 
-//4. Import the contacts.js file to create card components.
+// Aside from passing props like these, you can also pass them like special attributes as such:
+//  <Card name='Aras' imgURL='imagelink.com' phone='92358472590' email='me@me.com' />
